1b10516fae80410eea23dee6bb4dffebc91ea40f
Colton Voege <cvoege@gmail.com>
2014-09-26 03:31:34 -0400

    first commit

---
+++ background.js
@@ -0,0 +1,19 @@
+chrome.runtime.onInstalled.addListener(function() {
+	//chrome.storage.local.set(object items, function callback);
+	// chrome.storage.sync.set({'value': theValue}, function() {
+	// 	// Notify that we saved.
+	// 	message('Settings saved');
+	// });
+});
+chrome.app.runtime.onLaunched.addListener(function(){
+	chrome.app.window.create('../HTML/window.html', {
+		'bounds': {
+			'width': 1000,
+			'height': 800
+		},
+		"state": "maximized",
+	});
+});
+chrome.runtime.onSuspend.addListener(function() {
+  // Do some simple clean-up tasks.
+});
